e760716c6d6de417366061f8cc1b5b34fa484016
Yunzhi Liu <liu_yun_zhi@163.com>
2018-08-27 15:06:35 +0800

    [QiXin & LiuYunzhi] feat: completed header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Layout } from 'antd';
 import PageConten from './components/PageContent';
 import PageFooter from './components/PageFooter';
 import PageHeader from './components/PageHeader';
-import { Layout } from 'antd';
+import './App.css';
 
 class App extends Component {
   render() {
--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,26 +1,30 @@
-import { Layout, Menu} from 'antd';
+import { Layout, Dropdown, Menu, message, Button, Icon } from 'antd';
 import React from 'react';
 
-
 const { Header } = Layout;
+const menu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="1"><Icon type="setting" />个人中心</Menu.Item>
+    <Menu.Item key="2"><Icon type="logout" />退出</Menu.Item>
+  </Menu>
+);
 
+function handleMenuClick(e) {
+  message.info('Click on menu item.');
+  console.log('click', e);
+}
 
 const PageHeader = () => {
   return (
-    <Header className="header">
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        style={{ lineHeight: '64px' }}
-      >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
+    <Header className="header App-header">
+      <img alt="logo" src="./static/logo-white.763050c9.png" className="App-logo" />
+      <Dropdown overlay={menu}>
+        <Button style={{ float: 'right' }}>
+          User <Icon type="down" />
+        </Button>
+      </Dropdown>
     </Header>
-    );
+  );
 }
 
-  export default PageHeader;
+export default PageHeader;
