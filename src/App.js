c4e57ff7dddf013d9132e8154c6c4d3e9a7c80f1
Yunzhi Liu <liu_yun_zhi@163.com>
2018-08-27 13:31:06 +0800

    [QiXin & LiuYunzhi] refactor: split component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PageConten from './components/PageContent';
+import PageFooter from './components/PageFooter';
+import PageHeader from './components/PageHeader';
+import { Layout } from 'antd';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <PageHeader/>
+        <PageConten/>
+        <PageFooter/>
+      </Layout>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
-import Header from './components/PageHeader';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
